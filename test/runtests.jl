e4f0cdc9cb7272a559a34ad54f1924e6f90d37c9
Fergus Baker <fergusbkr@gmail.com>
2021-11-13 19:25:24 +0000

    Reverse Time (#5)
    * reverse time component
    
    * updated test for reversed time component

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,15 +6,15 @@ using Test, ComputedGeodesicEquations
     u = [0.0, 100.0, π / 2.0, 0.0]
     v = [0.0, -1.0, 0.0, 1e-4]
 
-    @test BoyerLindquistCoords.null_constrain(u, v, M, a) ≈ -1.0204101936749481
+    @test BoyerLindquistCoords.null_constrain(u, v, M, a) ≈ 1.020402030409642
 
-    v[1] = -1.0204101936749481
+    v[1] = 1.020402030409642
     @test all(
         BoyerLindquistCoords.geodesic_eq(u, v, M, a) .≈ (
-            -0.0002083677190516319,
-            -1.014389097284123e-7,
-            5.0373193274816055e-26,
-            1.958148390240991e-6,
+            0.00020812117198812312,
+            - 1.9798090919204276e-8,
+            -4.9607974159635116e-26,
+            2.0414383391937405e-6,
         ),
     )
 
